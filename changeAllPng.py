e47710bc08441f04e0ce17ce6604b19c9771c88c
jonhory <318075441@qq.com>
2016-11-16 14:22:50 +0800

    修改图片脚本

---
+++ changeAllPng.py
@@ -0,0 +1,36 @@
+import os
+import re
+import sys
+
+pattern = '([\w\W]*?)(@[0-9]+x)?(\.png)'
+dic = {}
+count = 0
+
+def changeName(path, nPrefix):
+    global count
+    global dic
+    global pattern
+    if os.path.isdir(path):
+        for filename in os.listdir(path):
+            if os.path.isfile(path + '/' + filename):
+                matched = re.search(pattern, filename)
+                if matched != None:
+                    prefix = matched.group(1)
+                    suffix = matched.group(2)
+                    enffix = matched.group(3)
+                    if suffix == None:
+                        suffix = ''
+                    if not dic.has_key(prefix):
+                        dic[prefix] = nPrefix + "_" + str(count)
+                        count += 1
+                    newName = dic[prefix] + suffix + enffix
+
+                    os.rename(path + '/' + filename, path + '/' + newName)
+            else:
+                changeName(path + '/' + filename, nPrefix)
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+    nPrefix = sys.argv[2]
+    changeName(path, nPrefix)
+    print 'changeName finish ,count == ' + str(count)
